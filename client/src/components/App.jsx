56da30898cf43bbe5d13db3b110ae47e032e46de
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-02-26 15:56:54 -0800

    implement simple state check

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 
 const App = () => {
 
+  const [clickedTotal, setClickedTotal] = useState(0);
+
+  const onIncrement = () => {
+    setClickedTotal(clickedTotal + 1);
+    console.log(clickedTotal);
+  };
+
   return (
-    <div>Hello World
+    <div>
+      <h1>Clicked</h1>
+      <h2>{clickedTotal}</h2>
+      <button onClick={onIncrement}>Increment</button>
     </div>
   );
 }
